ecf88f1f3a6b916076a1dabe7e70c3b5731e5142
Nikolay <n.grygoriev@gmail.com>
2020-03-20 15:08:54 +0300

    First block is ready

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.header__slider').slick({
+        infinite:true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="../image/arrow-left.svg" alt="arrow-left">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="../image/arrow-right.svg" alt="arrow-right">',
+        asNavFor:'.slider-dots'
+    });
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor:'.header__slider'
+    });
+});
